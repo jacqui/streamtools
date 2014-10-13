75a65e46b66ab6fde849a7c3b56732caa2efd875
Jacqui Maher <jacqui.maher@nytimes.com>
2014-10-13 16:33:43 -0400

    wip: adding ttl to set block

--- st/library/set.go
+++ st/library/set.go
@@ -2,6 +2,8 @@ package library
 
 import (
 	"errors"
+	"sync"
+	"time"
 
 	"github.com/nytlabs/gojee"                 // jee
 	"github.com/nytlabs/streamtools/st/blocks" // blocks
@@ -25,6 +27,60 @@ func NewSet() blocks.BlockInterface {
 	return &Set{}
 }
 
+type setItem struct {
+	Expires *time.Time
+}
+
+type stSet struct {
+	sync.Mutex
+
+	ttl      time.Duration
+	setItems map[interface{}]*setItem
+}
+
+func newStSet() *stSet {
+	s := &stSet{
+		setItems: map[interface{}]*setItem{},
+	}
+	return s
+}
+
+func (s *stSet) Add(k interface{}) error {
+	s.Lock()
+
+	expiresAt := time.Now().Add(s.ttl)
+
+	s.setItems[k] = &setItem{
+		Expires: &expiresAt,
+	}
+
+	s.Unlock()
+	return nil
+}
+
+func (s *stSet) IsMember(k interface{}) bool {
+	s.Lock()
+
+	_, ok := s.setItems[k]
+	if !ok {
+		return false
+	}
+
+	if s.setItems[k].Expired() {
+		delete(s.setItems, k)
+		return false
+	}
+	s.Unlock()
+	return true
+}
+
+func (i *setItem) Expired() bool {
+	if i.Expires == nil {
+		return false
+	}
+	return i.Expires.Before(time.Now())
+}
+
 // Setup is called once before running the block. We build up the channels and specify what kind of block this is.
 func (b *Set) Setup() {
 	b.Kind = "Core"
@@ -43,8 +99,12 @@ func (b *Set) Setup() {
 
 // Run is the block's main loop. Here we listen on the different channels we set up.
 func (b *Set) Run() {
-	var path string
-	set := make(map[interface{}]bool)
+	var path, ttlString string
+
+	var ttl time.Duration
+
+	set := newStSet()
+
 	var tree *jee.TokenTree
 	var err error
 	for {
@@ -57,6 +117,19 @@ func (b *Set) Run() {
 				b.Error(err)
 				break
 			}
+
+			ttlString, err = util.ParseString(ruleI, "TimeToLive")
+			if err != nil {
+				b.Error(err)
+				break
+			}
+			ttl, err = time.ParseDuration(ttlString)
+			if err != nil {
+				b.Error(err)
+				break
+			}
+			set.ttl = ttl
+
 		case <-b.quit:
 			// quit the block
 			return
@@ -73,8 +146,8 @@ func (b *Set) Run() {
 				b.Error(errors.New("can only build sets of strings"))
 				continue
 			}
-			set[v] = true
-			// deal with inbound data
+			set.Add(v)
+
 		case msg := <-b.isMember:
 			if tree == nil {
 				continue
@@ -84,18 +157,18 @@ func (b *Set) Run() {
 				b.Error(err)
 				break
 			}
-			_, ok := set[v]
 			b.out <- map[string]interface{}{
-				"isMember": ok,
+				"isMember": set.IsMember(v),
 			}
 		case c := <-b.cardinality:
 			c <- map[string]interface{}{
-				"cardinality": len(set),
+				"cardinality": len(set.setItems),
 			}
 		case c := <-b.queryrule:
 			// deal with a query request
 			c <- map[string]interface{}{
-				"Path": path,
+				"Path":       path,
+				"TimeToLive": ttlString,
 			}
 
 		}
